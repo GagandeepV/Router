f576d5fdb9ab301e23432d303f420f9be74dcb18
GagandeepV <gagandeep.veerabhadregowda@meredith.com>
2022-07-09 19:54:34 +0530

    router version change, css improve,page fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-      <nav>
+      <nav style={{display:'flex', justifyContent:'space-around'}}>
           <Link to="/" ><h3>Logo</h3></Link>
-          <ul>
+          <ul style={{listStyleType:'none' ,display:'flex', gap:'1em' }}>
               <Link to="/about" ><li>About</li></Link>
-              <Link to="/access" ><li>Access</li></Link>
+              <Link to="/profile" ><li>Profile</li></Link>
           </ul>
       </nav>
     )
